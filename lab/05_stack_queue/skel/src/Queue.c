0e54ca4cbd84d86a30b3c8d2cceab216f44ee3ec
student <gheorghisor.ileana@gmail.com>
2021-04-10 14:53:34 +0300

    laboratorul 5

--- lab/05_stack_queue/skel/src/Queue.c
+++ lab/05_stack_queue/skel/src/Queue.c
@@ -1,36 +1,114 @@
+/* Gheorghisor Ileana-Teodora, 313CA*/
 #include <stdlib.h>
+#include <string.h>
 
 #include "Queue.h"
+#include "utils.h"
 
-void init_q(struct Queue *q) {
-    q->list = malloc(sizeof(struct LinkedList));
-    init_list(q->list);
+
+queue_t *
+q_create(unsigned int data_size, unsigned int max_size)
+{
+	/* TODO */
+	queue_t *q = malloc(sizeof(queue_t));
+	q->buff = malloc(max_size * sizeof(void*));
+	for(unsigned int i = 0; i < max_size; i++) {
+		q->buff[i] = malloc(sizeof(data_size));
+	}
+	q->max_size = max_size;
+	q->size = 0;
+	q->data_size = data_size;
+	q->read_idx = 0;
+	q->write_idx = 0;
+
+	return q;
 }
 
-int get_size_q(struct Queue *q) {
-    /* TODO */
+unsigned int
+q_get_size(queue_t *q)
+{
+	/* TODO */
+	return q->size;
 }
 
-int is_empty_q(struct Queue *q) {
-    /* TODO */
+/*
+ * Intoarce 1 daca coada este goala si 0 in caz contrar.
+ */
+unsigned int
+q_is_empty(queue_t *q)
+{
+	/* TODO */
+	DIE(!q, "NULL queue\n");
+	DIE(!q->buff, "NULL buffer\n");
+
+	if (q->size == 0) {
+		return 1;
+	}
+	return 0;
 }
 
-void* front(struct Queue *q) {
-    /* TODO */
+void *
+q_front(queue_t *q)
+{
+	/* TODO */
+	return q->buff[q->read_idx];
 }
 
-void dequeue(struct Queue *q) {
-    /* TODO */
+/* Functia intoarce true daca operatia s-a efectuat si false in caz contrar */
+bool
+q_dequeue(queue_t *q)
+{
+	/* TODO */
+	DIE(!q, "NULL queue\n");
+	DIE(!q->buff, "NULL buffer\n");
+	if (q->size == 0) {
+		return false;
+	}
+	if (q->buff[q->read_idx] != NULL) {
+		q->size--;
+		q->read_idx = (q->read_idx + 1) % q->max_size; /*Pt. circularitate.*/
+		return true;
+	}
+	return false;
 }
 
-void enqueue(struct Queue *q, void *new_data) {
-    /* TODO */
+/* Functia intoarce true daca operatia s-a efectuat si false in caz contrar */
+bool
+q_enqueue(queue_t *q, void *new_data)
+{
+	/* TODO */
+	DIE(!q, "NULL queue\n");
+	if (q->size == q->max_size) {
+		printf("Queue is maxed!\n");
+		return false;
+	} else {
+		memcpy(q->buff[q->write_idx], new_data, q->data_size);
+		q->size++;
+		q->write_idx = (q->write_idx + 1) % q->max_size; /* Pt. circularitate. */
+		return true;
+	}
 }
 
-void clear_q(struct Queue *q) {
-    /* TODO */
+void
+q_clear(queue_t *q)
+{
+	/* TODO */
+	DIE(!q, "NULL queue\n");
+	DIE(!q->buff, "NULL buffer\n");
+	for(unsigned int i = 0; i < q->max_size; i++) {
+		free(q->buff[i]);
+	}
+	q->size = 0;
+	q->read_idx = 0;
+	q->write_idx = 0;
 }
 
-void purge_q(struct Queue *q) {
-    /* TODO */
+void
+q_free(queue_t *q)
+{
+	/* TODO */
+	DIE(!q, "NULL queue\n");
+	q_clear(q);
+	free(q->buff);
+	free(q);
 }
